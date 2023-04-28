43be35568d5322c13661c537ace66316ec8958ef
Abhi-goyal1 <abhigoyal72621289@gmail.com>
2023-04-28 22:27:49 +0530

    not

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var typed = new typed(".typing", {
+var typed = new Typed(".typing", {
     strings: [,`Full stack developer.`,`Full stack developer.`, `Web designer.`, `Web designer.`, `Web developer.`, `Mern Stack developer.`,`Android development.`,],
     typeSpeed: 100,
     BackSpeed: 80,
-    loop: true
+    loop:true
 })
+
